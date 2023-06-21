e9ad2f211d3103e6081d7db148ebb76839a583ea
zoegao <1060448102@qq.com>
2023-06-21 23:55:00 +0800

    2-7 增加hello测试接口test.http

--- src/main/java/com/knowl/wiki/controller/TestController.java
+++ src/main/java/com/knowl/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.knowl.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post" +name;
     }
 }
