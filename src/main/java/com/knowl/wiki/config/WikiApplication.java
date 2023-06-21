bced39c12f801b549e590681f8b46d49cbc5850d
zoegao <1060448102@qq.com>
2023-06-21 23:07:34 +0800

    2-6 hello world接口 http://127.0.0.1:8080/hello

--- src/main/java/com/knowl/wiki/config/WikiApplication.java
+++ src/main/java/com/knowl/wiki/config/WikiApplication.java
@@ -1,8 +1,10 @@
-package com.knowl.wiki;
+package com.knowl.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.knowl")
 @SpringBootApplication
 public class WikiApplication {
 
