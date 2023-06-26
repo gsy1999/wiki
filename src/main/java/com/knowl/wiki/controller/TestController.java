fa07c93921db41f22c207e90670cfdb925e9e354
zoegao <1060448102@qq.com>
2023-06-26 11:04:26 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/knowl/wiki/controller/TestController.java
+++ src/main/java/com/knowl/wiki/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.knowl.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.Hello:TEST}")  //冒号后面是默认值，如果没有扫描到配置文件的对应内容，则会显示默认值
+    private String testHello;
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!"+ testHello;
     }
 
     @PostMapping("/hello/post")
