e3f89ca0618869a1b519984f8c69a24af54a52e4
zoegao <1060448102@qq.com>
2023-06-27 21:39:40 +0800

    3-5 增加demo表列表查询

---
+++ src/main/java/com/knowl/wiki/service/DemoService.java
@@ -0,0 +1,18 @@
+package com.knowl.wiki.service;
+
+import com.knowl.wiki.domain.Demo;
+import com.knowl.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
