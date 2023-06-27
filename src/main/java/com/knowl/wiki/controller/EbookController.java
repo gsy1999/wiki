22b7b63ee684aff103fe3855366e605474a0548c
zoegao <1060448102@qq.com>
2023-06-28 00:05:37 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/knowl/wiki/controller/EbookController.java
+++ src/main/java/com/knowl/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.knowl.wiki.controller;
 
-import com.knowl.wiki.domain.Ebook;
+import com.knowl.wiki.req.EbookReq;
 import com.knowl.wiki.resp.CommonResp;
+import com.knowl.wiki.resp.EbookResp;
 import com.knowl.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/knowl/wiki/service/EbookService.java
+++ src/main/java/com/knowl/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.knowl.wiki.service;
 
 import com.knowl.wiki.domain.Ebook;
+import com.knowl.wiki.domain.EbookExample;
 import com.knowl.wiki.mapper.EbookMapper;
+import com.knowl.wiki.req.EbookReq;
+import com.knowl.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,7 +17,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebooklist = ebookMapper.selectByExample(ebookExample);//会根据ebookExample里面的条件，也就是andNameLike设置的查询语句
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebooklist) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);  //对象复制
+            respList.add(ebookResp);
+        }
+        return respList;
     }
+
 }
