41bfb473aba12177d91d06a153314394e1767810
zoegao <1060448102@qq.com>
2023-06-28 18:03:37 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/knowl/wiki/service/EbookService.java
+++ src/main/java/com/knowl/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.knowl.wiki.domain.EbookExample;
 import com.knowl.wiki.mapper.EbookMapper;
 import com.knowl.wiki.req.EbookReq;
 import com.knowl.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.knowl.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,13 +22,17 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebooklist = ebookMapper.selectByExample(ebookExample);//会根据ebookExample里面的条件，也就是andNameLike设置的查询语句
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebooklist) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);  //对象复制
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebooklist) {
+////            EbookResp ebookResp = new EbookResp();
+////           //对象复制
+// BeanUtils.copyProperties(ebook, ebookResp);
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+        List<EbookResp> list = CopyUtil.copyList(ebooklist, EbookResp.class);
+        return list;
     }
 
 }
