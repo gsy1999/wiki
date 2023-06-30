5e209ea612e580901d3683cac42beb345292850a
zoegao <1060448102@qq.com>
2023-07-01 01:19:28 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki电子书
+  </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
