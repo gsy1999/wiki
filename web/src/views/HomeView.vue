8948fe798cbd40519162a3374de40a330a146a89
zoegao <1060448102@qq.com>
2023-07-01 01:13:22 +0800

    4-7 制作the-header组件

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -14,7 +14,7 @@
                 subnav 1
               </span>
           </template>
-          <a-menu-item key="1">option11</a-menu-item>
+          <a-menu-item key="1">option</a-menu-item>
           <a-menu-item key="2">option2</a-menu-item>
           <a-menu-item key="3">option3</a-menu-item>
           <a-menu-item key="4">option4</a-menu-item>
