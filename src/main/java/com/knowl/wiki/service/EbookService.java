50f091a705a3adfa160d7802bb9d36dfa19f1e97
zoegao <1060448102@qq.com>
2023-07-03 22:36:41 +0800

    6-4 使用pagehelper实现后端分页

--- src/main/java/com/knowl/wiki/service/EbookService.java
+++ src/main/java/com/knowl/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.knowl.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.knowl.wiki.domain.Ebook;
 import com.knowl.wiki.domain.EbookExample;
 import com.knowl.wiki.mapper.EbookMapper;
 import com.knowl.wiki.req.EbookReq;
 import com.knowl.wiki.resp.EbookResp;
 import com.knowl.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,9 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -23,8 +30,12 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         };
-        List<Ebook> ebooklist = ebookMapper.selectByExample(ebookExample);//会根据ebookExample里面的条件，也就是andNameLike设置的查询语句
+        PageHelper.startPage(1,3);//这一行要跟最重要查询的语句放在一起，不然中间如果还有别的语句就会失效
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);//会根据ebookExample里面的条件，也就是andNameLike设置的查询语句
 
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebooklist) {
 ////            EbookResp ebookResp = new EbookResp();
@@ -34,7 +45,7 @@ public class EbookService {
 //            respList.add(ebookResp);
 //        }
 
-        List<EbookResp> list = CopyUtil.copyList(ebooklist, EbookResp.class);
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
         return list;
     }
 
