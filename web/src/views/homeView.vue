d32722cd37715782d3aa91f0bd28c1ad8a3dbafe
zoegao <1060448102@qq.com>
2023-07-05 22:30:33 +0800

    7-7 首页显示分类菜单

--- web/src/views/homeView.vue
+++ web/src/views/homeView.vue
@@ -3,45 +3,22 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-
-
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
+        <a-menu-item key="welcome">
+          <router-link to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -75,6 +52,8 @@
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue'; //下面要用什么方法先在这里引入
 // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from "axios";
+import { message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
 // const listData: any = [];
 //
@@ -95,11 +74,35 @@ export default defineComponent({
   setup(){
     // console.log("setup");
     const ebooks = ref();
-    const ebooks1 = reactive({books:[]}); //reactive后面通常放对象，这里放了一个空的对象，books是自己定义的属性
+    const level1 =  ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = (value: any) => {
+      console.log("menu click", value)
+      }
 
     onMounted(() => {
       //初始化逻辑都写到onMounted里面，setup就放一些参数定义，方法定义
       // console.log("onMounted");
+      handleQueryCategory();
       axios.get("/ebook/list",{
         params:{
           page: 1,
@@ -129,6 +132,9 @@ export default defineComponent({
         { type: 'LikeOutlined', text: '156' },
         { type: 'MessageOutlined', text: '2' },
       ],
+
+      handleClick,
+      level1,
     }
   }
 });
