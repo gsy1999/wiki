326fb2a323d203b0569de1723fe9c26b3b59b7a2
zoegao <1060448102@qq.com>
2023-07-07 02:01:41 +0800

    8-7 集成富文本框插件wangEditor@4.6.3，在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -87,6 +87,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -97,6 +100,7 @@ import axios from 'axios';
 import { message } from "ant-design-vue";
 import { Tool } from "@/util/tool";
 import {useRoute} from "vue-router";
+import E from "wangeditor";
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -246,6 +250,8 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const editor = new E('#content');
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -276,6 +282,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
     };
 
     /**
@@ -291,6 +300,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
     };
 
     onMounted(() => {
