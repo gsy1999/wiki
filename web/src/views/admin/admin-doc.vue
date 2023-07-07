94ae04bb243669cecae5f4630dbe779233e97717
zoegao <1060448102@qq.com>
2023-07-07 15:45:00 +0800

    8-9 让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -21,12 +21,14 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
               size="small"
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text: record }">
               {{record.sort}} {{text}}
@@ -150,7 +152,7 @@ export default defineComponent({
      * }]
      */
     const level1 = ref(); // 一级文档树，children属性就是二级文档
-    level1.value = [];
+    level1.value = []; //
 
     /**
      * 将某节点及其子孙节点全部置为disabled
