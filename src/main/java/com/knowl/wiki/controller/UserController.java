37475f77aa08cc49b60272d25646274fce0438dc
zoegao <1060448102@qq.com>
2023-07-08 21:23:41 +0800

    9-9 集成redis,登录成功后将登录信息放入redis中

--- src/main/java/com/knowl/wiki/controller/UserController.java
+++ src/main/java/com/knowl/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.knowl.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.knowl.wiki.req.UserLoginReq;
 import com.knowl.wiki.req.UserQueryReq;
 import com.knowl.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,31 @@ import com.knowl.wiki.resp.PageResp;
 import com.knowl.wiki.resp.UserLoginResp;
 import com.knowl.wiki.resp.UserQueryResp;
 import com.knowl.wiki.service.UserService;
+import com.knowl.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req){  //@Valid意为，这组参数要开启校验规则
@@ -58,6 +72,12 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes())); //变成一个32位16进制
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        //生成单点登录token，并放入redis中
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString()); //返给前端
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
