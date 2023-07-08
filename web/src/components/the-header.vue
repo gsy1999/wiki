f67abb636186e57680312305ec6c98143c755403
zoegao <1060448102@qq.com>
2023-07-09 01:51:41 +0800

    9-10 增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息 问题：用edge打开点击退出登录时会报ResizeObserver loop limit exceeded，用chrome则不会

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,6 +22,16 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+      >
+        <a class="login-menu" v-show="user.id">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
       <a class="login-menu" v-show="user.id">
         <span>您好：{{user.name}}</span>
       </a>
@@ -100,15 +110,15 @@ export default defineComponent({
     // 退出登录
     const logout = () => {
       console.log("退出登录开始");
-      // axios.get('/user/logout/' + user.value.token).then((response) => {
-      //   const data = response.data;
-      //   if (data.success) {
-      //     message.success("退出登录成功！");
-      //     store.commit("setUser", {});
-      //   } else {
-      //     message.error(data.message);
-      //   }
-      // });
+      axios.get('/user/logout/' + user.value.token).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登录成功！");
+          store.commit("setUser", {});
+        } else {
+          message.error(data.message);
+        }
+      });
     };
 
     return {
@@ -126,15 +136,15 @@ export default defineComponent({
 </script>
 
 <style>
-.logo {
-  width: 120px;
-  height: 31px;
-  /*background: rgba(255, 255, 255, 0.2);*/
-  /*margin: 16px 28px 16px 0;*/
-  float: left;
-  color: white;
-  font-size: 18px;
-}
+/*.logo {*/
+/*  width: 120px;*/
+/*  height: 31px;*/
+/*  !*background: rgba(255, 255, 255, 0.2);*!*/
+/*  !*margin: 16px 28px 16px 0;*!*/
+/*  float: left;*/
+/*  color: white;*/
+/*  font-size: 18px;*/
+/*}*/
 .login-menu {
   float: right;
   color: white;
