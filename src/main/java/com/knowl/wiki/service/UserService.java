c10f077a6f80812043276d892badb0fd5d1d1354
zoegao <1060448102@qq.com>
2023-07-08 16:12:34 +0800

    9-6 修改的时候不修改密码

--- src/main/java/com/knowl/wiki/service/UserService.java
+++ src/main/java/com/knowl/wiki/service/UserService.java
@@ -79,6 +79,7 @@ public class UserService {
         } else {
             // 更新
             user.setLoginName(null); //这样下面就不会更新用户名这个字段
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);  //加上Selective意思是，判断user属性有值才会更新，没有值就不更新这个字段，所以上面先把值清空
         }
     }
