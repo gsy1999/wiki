1ba839f49467cc94302ef3400763fd112dde5fec
zoegao <1060448102@qq.com>
2023-07-08 21:58:07 +0800

    9-9 增加redis测试接口

--- src/main/java/com/knowl/wiki/controller/TestController.java
+++ src/main/java/com/knowl/wiki/controller/TestController.java
@@ -2,22 +2,27 @@ package com.knowl.wiki.controller;
 
 import com.knowl.wiki.domain.Test;
 import com.knowl.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @Value("${test.Hello:TEST}:test")  //冒号后面是默认值，如果没有扫描到配置文件的对应内容，则会显示默认值
     private String testHello;
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -32,5 +37,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list(){
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
