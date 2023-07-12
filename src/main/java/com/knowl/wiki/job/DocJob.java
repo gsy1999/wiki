7a31f5609a466b55727551741337fe3ad34de7df
zoegao <1060448102@qq.com>
2023-07-12 16:20:02 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL，增加日志

--- src/main/java/com/knowl/wiki/job/DocJob.java
+++ src/main/java/com/knowl/wiki/job/DocJob.java
@@ -21,7 +21,12 @@ import javax.annotation.Resource;
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        // 增加日志流水号
+//        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒", System.currentTimeMillis() - start);
     }
 
  }
