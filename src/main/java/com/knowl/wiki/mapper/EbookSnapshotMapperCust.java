40a249995c5af5be31f431586861a9ae20572517
zoegao <1060448102@qq.com>
2023-07-14 17:14:02 +0800

    11-5增加定时任务，定时收集数据 要在xml里一次执行多个SQL的话，需要为数据库连接增加参数: &allowMultiQueries=true

---
+++ src/main/java/com/knowl/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,10 @@
+package com.knowl.wiki.mapper;
+
+public interface EbookSnapshotMapperCust {
+
+    public void genSnapshot();
+
+//    List<StatisticResp> getStatistic();
+//
+//    List<StatisticResp> get30Statistic();
+}
